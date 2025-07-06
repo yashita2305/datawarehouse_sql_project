191120d4a8b399dff638769629cd357a07c47a85
yashita2305 <136897381+yashita2305@users.noreply.github.com>
2025-07-06 23:47:41 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,79 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+-- Drop and Create silver.crm_cust_info
+DROP TABLE IF EXISTS crm_cust_info;
+CREATE TABLE crm_cust_info (
+    cst_id             INT,
+    cst_key            VARCHAR(50),
+    cst_firstname      VARCHAR(50),
+    cst_lastname       VARCHAR(50),
+    cst_marital_status VARCHAR(50),
+    cst_gndr           VARCHAR(50),
+    cst_create_date    DATE,
+    dwh_create_date    DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Drop and Create silver.crm_prd_info
+DROP TABLE IF EXISTS crm_prd_info;
+CREATE TABLE crm_prd_info (
+    prd_id          INT,
+    cat_id          VARCHAR(50),
+    prd_key         VARCHAR(50),
+    prd_nm          VARCHAR(50),
+    prd_cost        INT,
+    prd_line        VARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Drop and Create silver.crm_sales_details
+DROP TABLE IF EXISTS crm_sales_details;
+CREATE TABLE crm_sales_details (
+    sls_ord_num     VARCHAR(50),
+    sls_prd_key     VARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Drop and Create silver.erp_loc_a101
+DROP TABLE IF EXISTS erp_loc_a101;
+CREATE TABLE erp_loc_a101 (
+    cid             VARCHAR(50),
+    cntry           VARCHAR(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Drop and Create silver.erp_cust_az12
+DROP TABLE IF EXISTS erp_cust_az12;
+CREATE TABLE erp_cust_az12 (
+    cid             VARCHAR(50),
+    bdate           DATE,
+    gen             VARCHAR(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Drop and Create silver.erp_px_cat_g1v2
+DROP TABLE IF EXISTS erp_px_cat_g1v2;
+CREATE TABLE erp_px_cat_g1v2 (
+    id              VARCHAR(50),
+    cat             VARCHAR(50),
+    subcat          VARCHAR(50),
+    maintenance     VARCHAR(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
